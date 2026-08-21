86916cd72d8389a467d6a8cb6051ebe98605cfb4
Ashucod <ashishrshinde10@gmail.com>
2026-08-21 16:11:43 +0000

    Learned new concepts

--- basics/basics.cpp
+++ basics/basics.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -14,9 +14,59 @@ int main() {
 
     // cin string inputs only one word or we can say anything before first space
     //to get a full line as input we use getline
-    string str;
-    getline(cin, str);
-    cout << str;
+    // string str;
+    // getline(cin, str);
+    // cout << str;
+
+
+    //char
+
+    // IF-Else
+
+    //Switch case
+    // int day;
+    // cin >> day;
+
+    // switch(day){
+    //     case 1:
+    //         cout << "Monday\n";
+    //         break;
+    //     case 2:
+    //         cout << "Tuesday\n";
+    //         break;
+    //     case 3:
+    //         cout << "Wednesday\n";
+    //         break;
+    //     case 4:
+    //         cout << "Thursday\n";
+    //         break;
+    //     case 5:
+    //         cout << "Friday\n";
+    //         break;
+    //     case 6:
+    //         cout << "Saturday\n";
+    //         break;
+    //     case 7:
+    //         cout << "Sunday\n";
+    //         break;
+    //     default:
+    //         cout << "Wrong input :(\n";
+    //         break;
+    // }
+
+
+    // Arrays
+    // 1D Arrays
+    // int arr[5]; // Array of size 5 and index goes from 0 to 1;
+    // 2D Arrays
+    // int arr[3][5]; // Arrays with 15 containers and index starts from 0
+
+    // Strings
+    // Its also a type of array of chars
+
+    // string s = "Ashish";
+    // int len = s.size(); // length is 6
+    // cout << "Last alphabet is : " << s[len-1] << "\n";
 
     return 0;
 }
