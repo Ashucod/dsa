373238bb825953725220e83d6d9084331d86b43e
Ashucod <ashishrshinde10@gmail.com>
2026-08-23 05:06:18 +0000

    Added left aligned upper triangle

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Square/rectangle pattern
 void pat1(int n) {
     for (int i = 0; i < n; i++)
     {
@@ -12,9 +13,20 @@ void pat1(int n) {
     }
 }
 
+// left aligned lower triangle
 void pat2(int n) {
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+// left aligned upper triangle
+void pat3(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= (n-i+1); j++) {
             cout << " * ";
         }
         cout << endl;
@@ -28,7 +40,7 @@ int main() {
     for (int i = 1; i <= t; i++) {
         int n;
         cin >> n;
-        pat2(n);
+        pat3(n);
     }
 
 }
