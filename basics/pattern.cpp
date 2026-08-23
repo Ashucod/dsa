a7da4ff60ca69e94fa9bd7fb0c5eda9dc60505bd
Ashucod <ashishrshinde10@gmail.com>
2026-08-23 11:01:47 +0000

    Added hollow pyramid pattern

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -94,6 +94,30 @@ void pat6(int n) {
     }
 }
 
+// hollow pyramid
+void pat7(int n) {
+     for (int i = 0; i < n; i++) {
+        // space
+        for (int j = 0; j < n-i; j++) {
+            cout << "   ";
+        }
+        // stars
+        for (int j = 0; j < (2*i+1); j++) {
+            if (j == 0 || j == 2*i || i == n-1)
+                cout << " * ";
+            else
+                cout << "   ";
+        }
+
+        //space
+        for (int j = 0; j < n-i; j++) {
+            cout << "   ";
+        }
+
+        cout << endl;
+     }
+}
+
 int main() {
     int t;
     cout << "Enter no. of test cases : ";
@@ -101,7 +125,7 @@ int main() {
     for (int i = 1; i <= t; i++) {
         int n;
         cin >> n;
-        pat6(n);
+        pat7(n);
     }
 
 }
