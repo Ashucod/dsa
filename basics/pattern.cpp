2d5db0fd04a4ebcca549f9ee91ca771081997060
Ashucod <ashishrshinde10@gmail.com>
2026-08-24 18:24:54 +0000

    Tried the diamond pattern

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -118,6 +118,45 @@ void pat7(int n) {
      }
 }
 
+// Diamond star pattern
+void pat8(int n) {
+    for (int i = 0; i < n; i++) {
+        // space
+        for (int j = 0; j < n-i; j++) {
+            cout << "   ";
+        }
+        // stars
+        for (int j = 0; j < (2*i + 1); j++) {
+            cout << " * ";
+        }
+        // space
+        for (int j = 0; j < n-i; j++) {
+            cout << "   ";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 0; i < (n-1); i++) {
+        // space
+        for (int j = 0; j <= i+1; j++) {
+            cout << "   ";
+        }
+
+        // stars
+        for (int j = 0; j < (2*((n-1)-i) - 1); j++) {
+            cout << " * ";
+        }
+
+        // space
+        for (int j = 0; j <= i+1; j++) {
+            cout << "   ";
+        }
+
+        cout << endl;
+    }
+}
+
 int main() {
     int t;
     cout << "Enter no. of test cases : ";
@@ -125,7 +164,7 @@ int main() {
     for (int i = 1; i <= t; i++) {
         int n;
         cin >> n;
-        pat7(n);
+        pat8(n);
     }
 
 }
