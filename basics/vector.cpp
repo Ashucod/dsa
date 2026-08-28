4127157728d41a3669a179e0daa147fba39fe21d
Ashucod <ashishrshinde10@gmail.com>
2026-08-28 19:10:12 +0000

    Learned Iterators

--- basics/vector.cpp
+++ basics/vector.cpp
@@ -5,10 +5,14 @@ using namespace std;
 int main() {
     vector<int> v = {10, 20, 30, 40};
 
+
+    // Ways to print a vector 
+    // common/ basic way
     for(vector<int>::iterator it = v.begin(); it != v.end(); it++) {
         cout << *(it) << " ";
     }
 
+    // a shorter way but accessing the memory first
     cout << endl;
 
     for(auto it = v.begin(); it != v.end(); it++) {
@@ -17,11 +21,14 @@ int main() {
 
     cout << endl;
 
+    // shortest way and directly accesses the value in that memory
     for(auto it : v) {
         cout << it << " ";
     }
 
     cout << endl;
+
+    
 
     return 0;
 }
