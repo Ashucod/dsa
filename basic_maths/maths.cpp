15aca370f6e18b0c5c5217e606f0ce2abd43b8f8
Ashucod <ashishrshinde10@gmail.com>
2026-08-29 18:11:46 +0000

    Found divisors with another method with different time complexity and used vectors and STL

--- basic_maths/maths.cpp
+++ basic_maths/maths.cpp
@@ -68,11 +68,42 @@ void isAmstrong(int n) {
 
 void divisors(int n) {
     cout << "Divisors of " << n << " are ";
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0){
-            cout << i << " ";
+    // TC : O(N)
+    // for (int i = 1; i <= n; i++) {
+    //     if (n % i == 0){
+    //         cout << i << " ";
+    //     }
+    // }
+    // cout << endl;
+
+    // Another way of finding divisors/factors
+    // 1 x 36
+    // 2 x 18
+    // 3 x 12
+    // 4 x 9
+    // 6 x 6 _____ Below this these are same pairs but written is opposite order.
+    // 9 x 4        So what do we observe is we'll iterate loop till the square root of the number as like in this case is 6
+    // ..           And then use sort() function from STL on the vector storing the factors
+    //              Because without sort it will print like 1 36 2 18 3 12 4 9 6
+    vector<int> ls;
+
+    // TC : O(sqrt(N))
+    for (int i = 1; i <= sqrt(n); i++) { // we can also use i*i <= n if not square root but the TC will remain same
+        if(n % i == 0) {
+            ls.push_back(i);
+            if((n/i) != i) {
+                ls.push_back((n/i));
+            }
         }
     }
+
+    //Sorting the vector
+    // O(n log(n))
+    sort(ls.begin(), ls.end());
+
+    // O(number of factors)
+    for(auto it : ls) cout << it << " ";
+
     cout << endl;
 }
 
