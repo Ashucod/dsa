2c0429bb40989710d0a22ed2bc28dcbc541e0a13
Ashucod <ashishrshinde10@gmail.com>
2026-09-01 09:43:41 +0000

    Recursion 1 : basics

--- recursion/re1.cpp
+++ recursion/re1.cpp
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
 
 int main() {
-    
+    // What is recursion?
+    // when a function calls itself until a specific condition is fullfilled
+    // Such condition is known as base condition
+    // Stack overflow $ Stack space - it is the space where yet to be executed functions are stored.
+    // Recursion tree - visual representation in terms of f(). 
     return 0;
 }
