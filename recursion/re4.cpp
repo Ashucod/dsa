d46105658e3493f5178a50416c88014a546acd71
Ashucod <ashishrshinde10@gmail.com>
2026-09-05 07:07:17 +0000

    Checked whether a given string is palindrome or not using parameterised recursion

--- recursion/re4.cpp
+++ recursion/re4.cpp
@@ -37,15 +37,26 @@ void f(int i, int n, int arr[]) {
     f(i+1, n, arr);
 }
 
+int palindrome(int i, int n, string s) {
+    if(i >= n/2) return 1;
+    if(tolower(s[i]) != tolower(s[n-i-1])) return 0;
+    return palindrome(i+1, n, s);
+}
+
 int main() {
     // problems on recursion
     // reverse an array
     int arr[] = { 1, 2, 3, 4, 5};
     int n = sizeof(arr)/sizeof(int);
+    string s = "Madam";
+    int ns = sizeof(s)/sizeof(char);
     // reverse_arr(0, n-1, arr);   
     // Remember arrays are passed by reference by default
     
     // by another methd
     f(0, n, arr);
+    int i = palindrome(0, n, s);
+    if(i == 1) cout << "Palindrome\n";
+    else cout << "Not palindrome\n";
     return 0;
 }
